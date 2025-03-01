45fbc2d19207ba56676c101a61fada49d297af5e
jul213 <158296107+jul213@users.noreply.github.com>
2025-03-01 12:28:26 +0000

    creacion de funcion main

--- calculadora.kt
+++ calculadora.kt
@@ -13,3 +13,11 @@ fun resta(num1: Int , num2: Int) -> Int {
 fun suma(num1: Int , num2: Int) -> Int {
     return num1 + num2
 }
+
+fun main(){
+
+    println("bienvenido al sistema de calculadora con Kotlin")
+
+    println("escriba el primer numero: ")
+    println("escriba el segundo numero: ")
+}
