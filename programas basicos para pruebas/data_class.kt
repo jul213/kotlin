112bbd0ea63306dd9953475c3996619ae5632020
jul213 <158296107+jul213@users.noreply.github.com>
2025-03-05 08:26:52 +0000

    instacia de data class

--- programas basicos para pruebas/data_class.kt
+++ programas basicos para pruebas/data_class.kt
@@ -5,3 +5,11 @@ data class Contact (
     val tel: String,
     val description: String
 )
+
+
+fun main(){
+
+    val contact1 = Contact("juan","45987650","es una persona interesante")
+    val contact2 = Contact("sara","4595847","es una persona amable")
+    val contact3 = Contact("rodolfo","439650","es una persona normal")
+}
