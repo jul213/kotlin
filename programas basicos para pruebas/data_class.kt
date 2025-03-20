cbb7a9818408a9b48b73ba2f64b2bdbfa79404fa
jul213 <158296107+jul213@users.noreply.github.com>
2025-03-20 22:27:10 +0000

    creacion de una instancia de la data class Client

--- programas basicos para pruebas/data_class.kt
+++ programas basicos para pruebas/data_class.kt
@@ -19,4 +19,6 @@ fun main(){
     val contact1 = Contact("juan","45987650","es una persona interesante")
     val contact2 = Contact("sara","4595847","es una persona amable")
     val contact3 = Contact("rodolfo","439650","es una persona normal")
+
+    val Cliente1 = Client("juan", tel = "7225030")
 }
